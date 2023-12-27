ed853d37ade641d93852edda97e3811e1959a499
AdityaPourush <adityapourush11@outlook.com>
2023-12-27 18:49:23 +0530

    done styling

--- routing/src/Components/Navbar.jsx
+++ routing/src/Components/Navbar.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 const Navbar = ()=> {
     return(
         <>
-            <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-                <Link to={"/"}><h1>Kalvium</h1></Link>
-                <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-                    <Link to={"/about"}> <h1>About</h1> </Link>
-                    <Link to={"/contact"}><h1>Contact</h1></Link>
+            <div style={{display: "flex", justifyContent: "space-between", alignItems: "center", }}>
+                <Link to={"/"} style={{padding : "2vh",  color: "black" , textDecoration: "none"}}><h1>Kalvium</h1></Link>
+                <div style={{display: "flex", justifyContent: "space-between", alignItems: "center", }}>
+                    <Link to={"/about"} style={{padding : "2vh",  color: "black" , textDecoration: "none"}}> <h1>About</h1> </Link>
+                    <Link to={"/contact"} style={{padding : "2vh",  color: "black" , textDecoration: "none"}}><h1>Contact</h1></Link>
                 </div>
             </div>
         </>
